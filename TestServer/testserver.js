071ff52d6495fee94ac4676a7a4b34a225e9d45a
dev-onejun <wjjgjk@naver.com>
2022-05-12 16:55:32 +0900

    update testserver.js

--- TestServer/testserver.js
+++ TestServer/testserver.js
@@ -4,9 +4,12 @@ const PORT = 8080
 
 const print = require('./lib/print.js');
 
-app.post('/', (request, response) => {
+app.post('/user', (request, response) => {
     print.requested(request);
-    response.send("You've been requested to Weltried test server.")
+
+    data = {"statusCode": 1};
+    response.send(data);
+
     print.sended(request);
 })
 
@@ -21,6 +24,63 @@ app.post('/currentposition', (request, response) => {
     response.send(data);
     print.sended(request);
 })
+
+app.post('/measureresult', (request, response) => {
+    print.requested(request);
+
+    data = [
+        {
+            phone_number: '01023456789',
+            date: '20220512',
+            time: '165322',
+            position: 2,
+        },
+        {
+            phone_number: '01023456789',
+            date: '20220512',
+            time: '165324',
+            position: 1,
+        },
+        {
+            phone_number: '01023456789',
+            date: '20220512',
+            time: '165326',
+            position: 2,
+        },
+    ];
+
+    response.send(data);
+
+    print.requested(request);
+});
+
+app.post('/statistic', (request, response) => {
+    print.requested(request);
+
+    data = [
+        {
+            phone_number: '01012345678',
+            date: '20220512',
+            time: '073733',
+            position: 0,
+        },
+        {
+            phone_number: '01012345678',
+            date: '20220512',
+            time: '073735',
+            position: 0,
+        },
+        {
+            phone_number: '01012345678',
+            date: '20220512',
+            time: '073737',
+            position: 1
+        },
+    ];
+    response.send(data);
+
+    print.sended(request);
+});
 
 app.listen(PORT, () => {
     console.log(`Weltried test app listening on port ${PORT}`)
